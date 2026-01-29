57a506d041985d233dfacd9a88d3a1240daec4f4
nirmaljb <nirmaljyotib@gmail.com>
2026-01-29 19:44:32 +0530

    Added methods in Account.java

--- src/Account.java
+++ src/Account.java
@@ -9,13 +9,30 @@ public class Account {
         this.balance = initialAmount;
     }
 
+    protected void getCustomerDetails() {
+        System.out.println("Full Name : " + customer.getFullName());
+        System.out.println("Date of Birth : " + customer.getDob());
+    }
+
     protected String getAccountId() {
         return this.accountId;
     }
 
-    private void withdrawAmount(double amount) {
-        if(amount > this.balance) {
+    protected void withdrawAmount(double amount) {
+        if (amount > this.balance || amount < 1000) {
             throw new IllegalArgumentException("Amount should be within the bank balance");
         }
+        this.balance -= amount;
+    }
+
+    protected void depositAmount(double amount) {
+        if(amount >= Double.MAX_VALUE) {
+            throw new IllegalArgumentException("The Amount should be within Double datatype limit");
+        }
+        this.balance += amount;
+    }
+
+    protected double getBalance() {
+        return this.balance;
     }
 }
